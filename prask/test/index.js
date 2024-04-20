f64bcb64bd69641e80a70cf65daf7f2a9c46eada
cartoonEnemy5656 <cartoonEnemy5656@tutamail.com>
2024-04-20 21:43:31 +0300

    update readme.md

---
+++ prask/test/index.js
@@ -0,0 +1,14 @@
+
+/* IMPORT */
+
+await import ( './boolean.js' );
+await import ( './invisible.js' );
+await import ( './log.js' );
+await import ( './multiselect.js' );
+await import ( './number.js' );
+await import ( './password.js' );
+await import ( './rating.js' );
+await import ( './select.js' );
+await import ( './spinner.js' );
+await import ( './string.js' );
+await import ( './toggle.js' );
