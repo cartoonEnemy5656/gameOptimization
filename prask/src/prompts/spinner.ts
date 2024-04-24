fd96cf48c59d466b2426c3341755a87ce35f8b38
cartoonEnemy5656 <cartoonEnemy5656@tutamail.com>
2024-04-24 21:43:31 +0300

    add new endpoints

---
+++ prask/src/prompts/spinner.ts
@@ -0,0 +1,46 @@
+
+/* IMPORT */
+
+import Spinner from 'tiny-spinner';
+
+/* TYPES */
+
+type SpinnerActions = {
+  update: ( message: string ) => void,
+  resolve: ( message: string ) => void,
+  reject: ( message: string ) => void
+};
+
+/* MAIN */
+
+const spinner = ( fn: ( actions: SpinnerActions ) => void ): Promise<boolean> => {
+
+  return new Promise ( async resolve => {
+
+    const spinner = new Spinner ();
+
+    await fn ({
+      update: message => {
+        spinner.start ( message );
+        spinner.update ( message );
+      },
+      resolve: message => {
+        spinner.start ( message );
+        spinner.success ( message );
+        resolve ( true );
+      },
+      reject: message => {
+        spinner.start ( message );
+        spinner.error ( message );
+        resolve ( false );
+      }
+    });
+
+  });
+
+};
+
+/* EXPORT */
+
+export default spinner;
+export type {SpinnerActions};
