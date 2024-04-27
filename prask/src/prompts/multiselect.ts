e0afd55d6e9811145975559201d7d0dbe1256e5a
cartoonEnemy5656 <cartoonEnemy5656@tutamail.com>
2024-04-27 21:43:31 +0300

    fixed validation bugs

---
+++ prask/src/prompts/multiselect.ts
@@ -0,0 +1,37 @@
+
+/* IMPORT */
+
+import {identity} from '../utils';
+import pick from './pick';
+import type {PickOption} from './pick';
+
+/* TYPES */
+
+type MultiselectOptions<T> = {
+  message: string,
+  limit?: number,
+  min?: number,
+  max?: number,
+  options: (( query?: string ) => MultiselectOption<T>[] | string[]) | MultiselectOption<T>[] | string[],
+  searchable?: boolean,
+  validate?: ( value: T[] ) => string | boolean
+};
+
+type MultiselectOption<T> = PickOption<T>;
+
+/* MAIN */
+
+const multiselect = <T = string> ( options: MultiselectOptions<T> ): Promise<T[] | undefined> => {
+
+  return pick ({
+    ...options,
+    multiple: true,
+    transform: identity
+  });
+
+};
+
+/* EXPORT */
+
+export default multiselect;
+export type {MultiselectOptions, MultiselectOption};
