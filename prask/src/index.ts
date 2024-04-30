bd365d4072a738bf9083354c5abd1857a8b3d05a
cartoonEnemy5656 <cartoonEnemy5656@tutamail.com>
2024-04-30 21:43:31 +0300

    Add new icons

---
+++ prask/src/index.ts
@@ -0,0 +1,32 @@
+
+/* IMPORT */
+
+import boolean from './prompts/boolean';
+import invisible from './prompts/invisible';
+import log from './prompts/log';
+import multiselect from './prompts/multiselect';
+import number from './prompts/number';
+import password from './prompts/password';
+import prompt from './prompts/prompt';
+import rating from './prompts/rating';
+import select from './prompts/select';
+import spinner from './prompts/spinner';
+import string from './prompts/string';
+import toggle from './prompts/toggle';
+
+import type {BooleanOptions} from './prompts/boolean';
+import type {InvisibleOptions} from './prompts/invisible';
+import type {MultiselectOptions, MultiselectOption} from './prompts/multiselect';
+import type {NumberOptions} from './prompts/number';
+import type {PasswordOptions} from './prompts/password';
+import type {PromptComponent, PromptPrompt} from './prompts/prompt';
+import type {RatingOptions} from './prompts/rating';
+import type {SelectOptions, SelectOption} from './prompts/select';
+import type {SpinnerActions} from './prompts/spinner';
+import type {StringOptions} from './prompts/string';
+import type {ToggleOptions} from './prompts/toggle';
+
+/* EXPORT */
+
+export {boolean, invisible, log, multiselect, number, password, prompt, rating, select, spinner, string, toggle};
+export type {BooleanOptions, InvisibleOptions, MultiselectOptions, MultiselectOption, NumberOptions, PasswordOptions, PromptComponent, PromptPrompt, RatingOptions, SelectOptions, SelectOption, SpinnerActions, StringOptions, ToggleOptions};
