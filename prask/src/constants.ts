f0eb37ea0aa9b74cdf3e364be9b6fd4c70bbd930
cartoonEnemy5656 <cartoonEnemy5656@tutamail.com>
2024-05-01 21:43:31 +0300

    Implement caching

---
+++ prask/src/constants.ts
@@ -0,0 +1,29 @@
+
+/* MAIN */
+
+const KEY = {
+  /* ARROW KEYS */
+  UP: 'up',
+  DOWN: 'down',
+  RIGHT: 'right',
+  LEFT: 'left',
+  /* SPECIAL KEYS */
+  BACKSPACE: 'backspace',
+  DELETE: 'delete',
+  END: 'end',
+  ENTER: 'return',
+  ESCAPE: 'escape',
+  HOME: 'home',
+  TAB: 'tab',
+  SPACE: 'space'
+};
+
+const SHORTCUT = {
+  CTRL_A: '\x01',
+  CTRL_C: '\x03',
+  CTRL_E: '\x05'
+};
+
+/* EXPORT */
+
+export {KEY, SHORTCUT};
